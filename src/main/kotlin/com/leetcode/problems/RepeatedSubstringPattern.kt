c55f677d5b818af43e677f811e19c8f9c0b0bdf3
Vladimir Prudnikov <vladimir.prudnikov@mynd.co>
2023-06-04 17:03:26 -0700

    Repeated Substring Pattern (459)

--- src/main/kotlin/com/leetcode/problems/RepeatedSubstringPattern.kt
+++ src/main/kotlin/com/leetcode/problems/RepeatedSubstringPattern.kt
@@ -2,29 +2,15 @@ package com.leetcode.problems
 
 class RepeatedSubstringPattern {
     fun repeatedSubstringPattern(s: String): Boolean {
-
         var i = 2;
         var found = false
 
         while (i <= s.length && !found) {
-
             if (s.length % i == 0) {
-
-                val len = s.length / i
-                val expected = s.substring(0, len)
-
-                found = true;
-
-                for (j in 1..i - 1) {
-                    if (s.substring(j * len, (j + 1) * len) != expected)
-                        found = false
-                }
+                found = s.substring(0, s.length / i).repeat(i) == s
             }
-
             i++
         }
-
         return found
     }
-
 }
