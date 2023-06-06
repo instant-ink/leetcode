9c68e7ce695ac8517a60aca9602d2f89b1d98302
Vladimir Prudnikov <vladimir.prudnikov@mynd.co>
2023-06-05 23:43:51 -0700

    Find the Index of the First Occurrence in a String (28)

---
+++ src/main/kotlin/com/leetcode/problems/FindTheIndexOfTheFirstOccurrenceInAString.kt
@@ -0,0 +1,7 @@
+package com.leetcode.problems
+
+class FindTheIndexOfTheFirstOccurrenceInAString {
+    fun strStr(haystack: String, needle: String): Int {
+        return haystack.indexOf(needle)
+    }
+}
